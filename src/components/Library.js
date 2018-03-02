826bfd3f143962a044f6355b32d4094862db05ad
David Hirst <34040303+drwhirstyburgers@users.noreply.github.com>
2018-03-02 12:55:15 -0500

    albums rendered

--- src/components/Library.js
+++ src/components/Library.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
+import albumData from './../data/albums';
 
 class Library extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { albums: albumData };
+  }
+
   render() {
     return (
       <section className="library">
-        Library will go here
+        {
+            this.state.albums.map( (album, index) =>
+              <div key ={index} >
+                {albums.title}
+              </div>
+            )
+         }
       </section>
     );
   }
