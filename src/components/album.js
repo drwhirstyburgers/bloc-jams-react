4f5461d4d54a6d3648e46d2f8ee62ab0978a17ac
David Hirst <34040303+drwhirstyburgers@users.noreply.github.com>
2018-03-07 17:23:31 -0500

    render currentTime and duration in PlayerBar

--- src/components/album.js
+++ src/components/album.js
@@ -22,6 +22,25 @@ class Album extends Component {
     this.audioElement.src = album.songs[0].audioSrc;
   }
 
+  componentDidMount() {
+    this.eventListeners = {
+      timeupdate: e => {
+        this.setState({ currentTime: this.audioElement.currentTime });
+      },
+      durationchange: e => {
+        this.setState({ duration: this.audioElement.duration });
+      }
+    };
+    this.audioElement.addEventListener('timeupdate', this.eventListeners.timeupdate);
+    this.audioElement.addEventListener('durationchange', this.eventListeners.durationchange);
+  }
+
+  componentWillUnmount() {
+    this.audioElement.src = null;
+    this.audioElement.removeEventListener('timeupdate'. this.eventListeners.timeupdate );
+    this.audioElement.removeEventListener('durationchange', this.eventListeners.durationchange);
+  }
+
   play() {
     this.audioElement.play();
     this.setState({ isPlaying: true });
