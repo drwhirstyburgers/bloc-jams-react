621886c6ac43d7c628c5154506dc20e5c8ee3787
David Hirst <34040303+drwhirstyburgers@users.noreply.github.com>
2018-08-07 16:36:00 -0400

    update images

--- src/components/PlayerBar.js
+++ src/components/PlayerBar.js
@@ -4,7 +4,7 @@ import '.././styles/PlayerBar.css';
 class PlayerBar extends Component {
   render() {
     return (
-      <section className="player-bar" align="center">
+      <section className="player-bar">
         <section id="buttons">
           <button id="previous" onClick={this.props.handlePrevClick}>
             <span className="ion-skip-backward"></span>
